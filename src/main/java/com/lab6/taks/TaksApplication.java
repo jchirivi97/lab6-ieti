4468514ea4bf6b465eb710ae6ccc858f0f6f30fa
jchirivi97 <jimmynivia@outlook.es>
2021-03-10 23:24:16 -0500

    creacion de proyecto

---
+++ src/main/java/com/lab6/taks/TaksApplication.java
@@ -0,0 +1,13 @@
+package com.lab6.taks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab6/taks/TaksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab6.taks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
